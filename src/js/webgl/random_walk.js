4570118b0a64e3f2167e733fa115c42b98f3fcab
Ed Ye <hahahadude@gmail.com>
2016-04-03 23:19:52 -0400

    Temporary homepage.

--- src/js/webgl/random_walk.js
+++ src/js/webgl/random_walk.js
@@ -1,51 +1,91 @@
 (function (){
-    var camera, scene, renderer, material
-    var date, prev_time, dt, line_update_time, update_counter
 
-    init()
-    animate()
+    var camera, scene, renderer;
 
-    function add_line(){
-        var line, pos, line_geo, line_mat;
-        line_geo = new THREE.Geometry();
+    var line;
+    var MAX_POINTS = 1000;
+    var draw_count;
 
-        line_geo.vertices[0] = new THREE.Vector3(0.0, 0.0, 0.0);
-        line_geo.vertices[1] = new THREE.Vector3(100, 100, 100);
+    init();
+    animate();
 
-        line_geo.colors = [ new THREE.Color(0xffffff),
-                            new THREE.Color(0xffffff)];
+    function init(){
+        renderer = new THREE.WebGLRenderer();
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
 
-        line_mat = new THREE.LineBasicMaterial(
-            {
-                color: 0xffffff,
-                opacity: 0.1,
-                linewidth: 3,
-                vertexColors: THREE.VertexColors
-            });
+        scene = new THREE.Scene();
 
-        line = new THREE.Line (line_geo, line_mat);
-        line.scale.x = line.scale.y = line.scale.z = 1.0;
-        line.position = THREE.Vector3(0,0,0);
+        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+
+        camera.position.set(0, 0, 1000);
+
+        var geometry = new THREE.BufferGeometry();
+
+        var positions = new Float32Array(MAX_POINTS * 3);
+
+        geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
+
+        draw_count = 2;
+        geometry.setDrawRange(0, draw_count);
+
+        var material = new THREE.LineBasicMaterial({ color : 0xff0000, linewidth: 3});
+
+        line = new THREE.Line(geometry, material);
         scene.add(line);
+
+        update_positions();
     }
 
-    function init (){
-        date = new Date()
-        prev_time = date.getTime()
+    function update_positions(){
+        var positions = line.geometry.attributes.position.array;
 
+        var x, y, z, index;
+        x = y = z = index = 0;
 
+        function clamp(n, min, max){
+            return Math.max(min, Math.min(max, n));
+        }
+
+        for( var i = 0, l = MAX_POINTS; i < l; i++){
+            positions[index++] = x;
+            positions[index++] = y;
+            positions[index++] = z;
+
+            x += ( Math.random() - 0.5) * 30;
+            y += ( Math.random() - 0.5) * 30;
+            z += ( Math.random() - 0.5) * 30;
+
+            var w, h, d;
+            w = window.innerWidth/2;
+            h = window.innerHeight/2;
+            d = 100;
+            x = clamp(x, -w, w);
+            y = clamp(y, -h, h);
+            z = clamp(z, -d, d);
+        }
     }
 
-    function update (){
-        dt = date.getTime() - prev_time
-        update_counter += dt
+    function update_line(){
+        draw_count = (draw_count + 1) % MAX_POINTS;
 
-        if (update_counter >= line_update_time){
-            add_line()
+        line.geometry.setDrawRange(0, draw_count);
+
+        if (draw_count === 0){
+            update_positions();
+
+            line.geometry.attributes.position.needsUpdate = true;
+
+            line.material.color.setHSL( Math.random(), 1, 0.5);
         }
     }
 
     function animate () {
-    }
-}())
+        requestAnimationFrame(animate);
 
+        update_line();
+
+        renderer.render(scene, camera);
+    }
+}());
